202c4d27a144ee7cae28dc63fee21c22132ebd8b
Nicolas Gonzalvez <ngonzalvez@sophilabs.com>
2015-07-06 20:24:17 -0300

    Create the story

--- unchaindjango/__init__.py
+++ unchaindjango/__init__.py
@@ -5,3 +5,12 @@ Unchain Django - PySchool
 __author__ = 'Sophilabs'
 __version__ = '0.0.1'
 __licence__ = 'MIT'
+
+from story.story import BaseStory
+
+class Story(BaseStory):
+
+    name = 'unchaindjango'
+    title = 'Unchain Django'
+    adventures = [
+    ]
