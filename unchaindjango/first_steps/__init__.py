8e0afa143803dd2961eb40f7ca11f8b8dbbbb539
Pablo Ricco <pricco@gmail.com>
2015-07-07 02:31:13 -0300

    Fix issue with django not installed.

--- unchaindjango/first_steps/__init__.py
+++ unchaindjango/first_steps/__init__.py
@@ -1,16 +1,17 @@
 import os
 import sys
 
-import django
-from django.test import Client
 from story.adventures import BaseAdventure
 
 
 class Adventure(BaseAdventure):
 
-    title = 'Hello World'
+    title = 'First Steps2'
 
     def test(self, file):
+        import django
+        from django.test import Client
+
         sys.path.insert(0, os.getcwd())
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unchained.settings')
 
